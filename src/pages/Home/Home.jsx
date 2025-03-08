b51540bf3f06e484a2298e671ad114b6837fbf5c
Nebil-Abdulfetah <159676747+Nebil-Abdulfetah@users.noreply.github.com>
2025-03-08 23:00:59 +0300

    List of movies based on categories

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/banner/Banner'
+import RowList from '../../components/rows/RowList/RowList'
 function Home() {
   return (
     <>
         <Header />
         <Banner />
+        <RowList />
         <Footer />
     </>
   )
--- src/utils/requests.jsx
+++ src/utils/requests.jsx
@@ -6,7 +6,7 @@ const requests =
     fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrornMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
     fetchTvShows: `/discover/movie?api_key=${API_KEY}&with_genres=1`,
